4932b6e4ed411205c0f172583309eb23dec5f87a
balay <dlolhd@gmail.com>
2022-05-22 12:31:37 +0200

    Commit de Domain Layer(Capa del dominio o reglas de negocio).

--- app/src/main/java/com/diego/kotlin/coupons/common/utils/Constants.kt
+++ app/src/main/java/com/diego/kotlin/coupons/common/utils/Constants.kt
@@ -1,4 +1,7 @@
 package com.diego.kotlin.coupons.common.utils
 
 object Constants {
+    const val ERROR_EXIST = "Error 01"
+    const val ERROR_LENGTH = "Error 02"
+    const val ERROR_UNKNOWN = "unknown"
 }
