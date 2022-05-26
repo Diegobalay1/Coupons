8d67e1aa12ca7d92c6ee4e4343ca18f6b504b097
balay <dlolhd@gmail.com>
2022-05-26 12:26:21 +0200

    Commit de UI Layer(State Holder)(Capa de la vista). También UI Layer(Context/Controller).

--- app/src/main/java/com/diego/kotlin/coupons/mainModule/view/MainActivity.kt
+++ app/src/main/java/com/diego/kotlin/coupons/mainModule/view/MainActivity.kt
@@ -2,11 +2,66 @@ package com.diego.kotlin.coupons.mainModule.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import com.diego.kotlin.coupons.R
+import com.diego.kotlin.coupons.common.entities.CouponEntity
+import com.diego.kotlin.coupons.common.utils.hideKeyboard
+import com.diego.kotlin.coupons.databinding.ActivityMainBinding
+import com.diego.kotlin.coupons.mainModule.viewModel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViewModel()
+        setupObservers()
+        setupButtons()
+    }
+
+    private fun setupViewModel() {
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    private fun setupObservers() {
+        mainViewModel.getResult().observe(this) { coupon ->
+            if (coupon == null) {
+                binding.tilDescription.hint =  getString(R.string.main_hint_description)
+                binding.tilDescription.isEnabled = true
+                binding.btnCreate.visibility = View.VISIBLE
+            } else {
+                binding.etDescription.setText(coupon.description)
+                val status = getString(if (coupon.isActive) R.string.main_hint_active else R.string.main_hint_description)
+                binding.tilDescription.hint = status
+                binding.tilDescription.isEnabled = false
+                binding.btnCreate.visibility = if (coupon.isActive) View.GONE else View.VISIBLE
+            }
+        }
+
+        mainViewModel.getSnackbarMsg().observe(this) { msg ->
+            Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setupButtons() {
+        binding.btnConsult.setOnClickListener {
+            mainViewModel.consultCouponByCode(binding.etCoupon.text.toString())
+            hideKeyboard(this, binding.root)
+        }
+
+        binding.btnCreate.setOnClickListener {
+            val coupon = CouponEntity(
+                code = binding.etCoupon.text.toString(),
+                description = binding.etDescription.text.toString())
+            mainViewModel.saveCoupon(coupon)
+            hideKeyboard(this, binding.root)
+        }
     }
 }
--- app/src/main/java/com/diego/kotlin/coupons/mainModule/viewModel/MainViewModel.kt
+++ app/src/main/java/com/diego/kotlin/coupons/mainModule/viewModel/MainViewModel.kt
@@ -1,4 +1,38 @@
 package com.diego.kotlin.coupons.mainModule.viewModel
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.diego.kotlin.coupons.R
+import com.diego.kotlin.coupons.common.entities.CouponEntity
+import com.diego.kotlin.coupons.common.utils.getMsgErrorByCode
+import com.diego.kotlin.coupons.mainModule.model.MainRepository
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private val repository = MainRepository()
+
+    private val result = MutableLiveData<CouponEntity>()
+    fun getResult() = result
+
+    private val snackbarMsg = MutableLiveData<Int>()
+    fun getSnackbarMsg() = snackbarMsg
+
+    fun consultCouponByCode(code: String) {
+        viewModelScope.launch {
+            result.value = repository.consultCouponByCode(code)
+        }
+    }
+
+    fun saveCoupon(couponEntity: CouponEntity){
+        viewModelScope.launch {
+            try {
+                repository.saveCoupon(couponEntity)
+                consultCouponByCode(couponEntity.code)
+                snackbarMsg.value = R.string.main_save_success
+            } catch (e: Exception) {
+                snackbarMsg.value = getMsgErrorByCode(e.message)
+            }
+        }
+    }
 }
